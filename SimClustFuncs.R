9b34fa6b2a83ea8864d92b9b4d249daa61b35e1e
MCnu <mchas92@gmail.com>
2020-11-19 14:44:35 -0600

    Fixed acos bug in cluster analysis, removed is_bound

--- SimClustFuncs.R
+++ SimClustFuncs.R
@@ -17,15 +17,31 @@ sim_clust_interdist <- function(primary_xy, secondary_xy) {
   return(interdist_output_matlist)
 }
 
-sim_clust_magcor <- function(primary_d2p, secondary_d2p) {
-  plen <- length(primary_d2p)
-  slen <- length(secondary_d2p)
+sim_clust_magcor <- function(primary_xy, secondary_xy) {
+  plen <- length(primary_xy)
+  slen <- length(secondary_xy)
   if (plen != slen) stop("lists must have equal length")
+
+  sub_plen <- nrow(primary_xy[[1]])
+  sub_slen <- nrow(secondary_xy[[1]])
+  if (sub_plen != sub_slen) stop("unequal d2p sub matrix")
 
   cor_output_mat <- matrix(data = NA, nrow = plen, ncol = 2)
 
   for (i in 1:plen) {
-    float_cor <- cor(as.vector(primary_d2p[[i]]), as.vector(secondary_d2p[[i]]))
+    # initiate d2p matrix
+    d2p_matrix <- matrix(data = NA, nrow = sub_plen - 1, ncol = 2)
+
+    for (j in 2:sub_plen) {
+      pri_x <- (primary_xy[[i]][j, 1] - primary_xy[[i]][(j - 1), 1])
+      sec_x <- (secondary_xy[[i]][j, 1] - secondary_xy[[i]][(j - 1), 1])
+      pri_y <- (primary_xy[[i]][j, 2] - primary_xy[[i]][(j - 1), 2])
+      sec_y <- (secondary_xy[[i]][j, 2] - secondary_xy[[i]][(j - 1), 2])
+      p_leg <- sqrt(pri_x^2 + pri_y^2)
+      s_leg <- sqrt(sec_x^2 + sec_y^2)
+      d2p_matrix[j - 1, ] <- c(p_leg, s_leg)
+    }
+    float_cor <- cor(d2p_matrix[, 1], d2p_matrix[, 2])
     cor_output_mat[i, c(1, 2)] <- c(i, float_cor)
   }
 
@@ -35,39 +51,42 @@ sim_clust_magcor <- function(primary_d2p, secondary_d2p) {
   return(cor_output_df)
 }
 
-sim_clust_theta <- function(primary_d2p, secondary_d2p, stepwise_interdist) {
-  plen <- length(primary_d2p)
-  slen <- length(secondary_d2p)
-  ilen <- length(stepwise_interdist)
+sim_clust_theta <- function(primary_xy, secondary_xy) {
+  plen <- length(primary_xy)
+  slen <- length(secondary_xy)
   if (plen != slen) stop("lists must have equal length")
-  if (plen != ilen) stop("lists must have equal length")
 
-  sub_plen <- length(primary_d2p[[1]])
-  sub_slen <- length(secondary_d2p[[1]])
-  sub_ilen <- nrow(stepwise_interdist[[1]])
+  sub_plen <- nrow(primary_xy[[1]])
+  sub_slen <- nrow(secondary_xy[[1]])
+
   if (sub_plen != sub_slen) stop("unequal d2p sub matrix")
-  if ((sub_plen + 1) != sub_ilen) stop("interdist should be one row greater than d2p")
 
   theta_output_list <- lapply(1:plen, matrix, data = NA, nrow = sub_plen, ncol = 1)
 
   for (i in 1:plen) {
-    for (j in 1:sub_plen) {
-      p_leg <- as.numeric(primary_d2p[[i]][j])
-      s_leg <- as.numeric(secondary_d2p[[i]][j])
-      i_leg <- as.numeric(stepwise_interdist[[i]][j + 1])
+    for (j in 2:sub_plen) {
+      pri_x <- (primary_xy[[i]][j, 1] - primary_xy[[i]][(j - 1), 1])
+      sec_x <- (secondary_xy[[i]][j, 1] - secondary_xy[[i]][(j - 1), 1])
+      pri_y <- (primary_xy[[i]][j, 2] - primary_xy[[i]][(j - 1), 2])
+      sec_y <- (secondary_xy[[i]][j, 2] - secondary_xy[[i]][(j - 1), 2])
+      p_leg <- sqrt(pri_x^2 + pri_y^2)
+      s_leg <- sqrt(sec_x^2 + sec_y^2)
+      i_leg <- sqrt((pri_x - sec_x)^2 + (pri_y - sec_y)^2)
 
-      # law of cosines
-      float_acos_arg <- (-(i_leg^2 - p_leg^2 - s_leg^2)) / (2 * p_leg * s_leg)
+      if (p_leg < 0.001 | s_leg < 0.001) {
+        next
+      }
 
-      if (float_acos_arg > 1) float_acos_arg <- 1
-
-      if (float_acos_arg < -1) float_acos_arg <- -1
+      float_acos_arg <- (i_leg^2 - p_leg^2 - s_leg^2) / (-1 * 2 * p_leg * s_leg)
 
       float_ang <- acos(float_acos_arg)
 
-      if (is.nan(float_ang)) print(float_acos_arg)
-
-      theta_output_list[[i]][j, 1] <- float_ang
+      if (is.nan(float_ang)) {
+        print(c(float_acos_arg, p_leg, s_leg, i_leg))
+        next
+      } else {
+        theta_output_list[[i]][(j - 1), 1] <- float_ang
+      }
     }
   }
 
